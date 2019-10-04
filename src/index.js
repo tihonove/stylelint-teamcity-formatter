0762fd90cef4298b7227cdbe0bf6e2917b3d1012
fresk <fresk@yandex-team.ru>
2019-10-04 19:17:51 +0300

    Replace console.log with string return

--- src/index.js
+++ src/index.js
@@ -27,26 +27,37 @@ function groupBy(array, propertyPicker) {
 const reportName = 'StyleLint Violations';
 
 export default function teamcityFormatter(allResults) {
+    const output = [];
+
     allResults.forEach(x => {
         x.source = x.source.replace(process.cwd(), '').replace(/^\\/, '');
     });
-    console.log(`##teamcity[testSuiteStarted name='${reportName}']`);
+
+    output.push(`##teamcity[testSuiteStarted name='${reportName}']`);
+
     const groupedResults = groupBy(allResults, x => x.source);
 
     for (const results of groupedResults) {
         const testNameEscaped = reportName + ': ' + escapeTeamCityString(results[0].source);
-        console.log(`##teamcity[testStarted name='${testNameEscaped}']`);
+
+        output.push(`##teamcity[testStarted name='${testNameEscaped}']`);
+
         if (results.reduce((x, y) => x || y.errored, false)) {
             const message = results
                 .map(result => result.warnings
                     .map(warning => `${warning.text} at (${warning.line}, ${warning.column})`)
                     .join('\n'))
                 .join('\n');
-            console.log(
+
+            output.push(
                 `##teamcity[testFailed name='${testNameEscaped}'` +
                 ` message='${escapeTeamCityString(message)}']`);
         }
-        console.log(`##teamcity[testFinished name='${testNameEscaped}']`);
+
+        output.push(`##teamcity[testFinished name='${testNameEscaped}']`);
     }
-    console.log(`##teamcity[testSuiteFinished name='${reportName}']`);
+
+    output.push(`##teamcity[testSuiteFinished name='${reportName}']`);
+
+    return output.join('\n');
 }
